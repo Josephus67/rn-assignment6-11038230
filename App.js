90999e1b25968f83cc6801a96c6f20b09b1fa94b
Josephus67 <jbawah002@st.ug.edu.gh>
2024-06-29 10:09:48 -0700

    items of the menu row

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Safe, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View,SafeAreaView, Image } from 'react-native';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <View>
-      <Text style={{ fontSize: 25}}>Open Fashion </Text>
+        <View style={styles.first_view}>
+        <Image source={require('./assets/Menu.png')} />
+        <View>
+        <Text style={{ fontSize: 25, fontWeight: "bold"}}>Open</Text>
+        <Text style={{ fontSize: 25, fontWeight: "bold"}}>Fashion</Text>
+        </View>
+        <View style={{flexDirection: 'row', gap: 15}}>
+       <Image source={require('./assets/Search.png')} />
+        <Image source={require('./assets/shoppingBag.png')} />
+        </View>
+        </View>
+      
+
       <Text style={{ fontSize: 25}}>Our Story</Text>
 
 </View>
@@ -18,6 +30,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-   
+   justifyContent: 'space-evenly',
+   paddingLeft: 20,
+   paddingRight: 20,
   },
+  first_view: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+    }
 });
